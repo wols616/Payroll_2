1384941e63271a57ad8c834121dfb28898616ab2
wols616 <wilber.rivas1@catolica.edu.sv>
2025-02-26 20:59:25 -0600

    por favor que funcione

--- Payroll_1/Formularios/Admin/AgregarCategoria.Designer.cs
+++ Payroll_1/Formularios/Admin/AgregarCategoria.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             dgvCategoria = new DataGridView();
             agregar = new Button();
             button3 = new Button();
@@ -38,13 +40,26 @@
             btnAgregarPuesto = new Button();
             pictureBox1 = new PictureBox();
             label3 = new Label();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)dgvCategoria).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // dgvCategoria
             // 
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(183, 171, 240);
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(135, 115, 238);
+            dgvCategoria.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             dgvCategoria.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(90, 63, 230);
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dgvCategoria.DefaultCellStyle = dataGridViewCellStyle2;
             dgvCategoria.Location = new Point(74, 87);
             dgvCategoria.Name = "dgvCategoria";
             dgvCategoria.RowHeadersWidth = 51;
@@ -56,22 +71,32 @@
             // 
             // agregar
             // 
-            agregar.Location = new Point(474, 463);
+            agregar.BackColor = Color.FromArgb(135, 115, 238);
+            agregar.FlatAppearance.BorderSize = 0;
+            agregar.FlatStyle = FlatStyle.Flat;
+            agregar.Font = new Font("Bahnschrift", 10.2F, FontStyle.Bold);
+            agregar.ForeColor = Color.White;
+            agregar.Location = new Point(380, 463);
             agregar.Name = "agregar";
-            agregar.Size = new Size(143, 29);
+            agregar.Size = new Size(225, 40);
             agregar.TabIndex = 1;
             agregar.Text = "Agregar";
-            agregar.UseVisualStyleBackColor = true;
+            agregar.UseVisualStyleBackColor = false;
             agregar.Click += agregar_Click;
             // 
             // button3
             // 
-            button3.Location = new Point(325, 463);
+            button3.BackColor = Color.FromArgb(135, 115, 238);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Bahnschrift", 10.2F, FontStyle.Bold);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(122, 463);
             button3.Name = "button3";
-            button3.Size = new Size(143, 29);
+            button3.Size = new Size(220, 40);
             button3.TabIndex = 3;
             button3.Text = "Editar";
-            button3.UseVisualStyleBackColor = true;
+            button3.UseVisualStyleBackColor = false;
             button3.Click += button3_Click;
             // 
             // TxtNombreCat
@@ -84,18 +109,20 @@
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label1.Location = new Point(86, 387);
             label1.Name = "label1";
-            label1.Size = new Size(173, 20);
+            label1.Size = new Size(179, 20);
             label1.TabIndex = 7;
             label1.Text = "Nombre de la Categoria:";
             // 
             // label2
             // 
             label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label2.Location = new Point(86, 414);
             label2.Name = "label2";
-            label2.Size = new Size(93, 20);
+            label2.Size = new Size(97, 20);
             label2.TabIndex = 8;
             label2.Text = "Sueldo Base:";
             // 
@@ -108,19 +135,24 @@
             // 
             // btnAgregarPuesto
             // 
-            btnAgregarPuesto.Location = new Point(476, 527);
+            btnAgregarPuesto.BackColor = Color.FromArgb(90, 63, 230);
+            btnAgregarPuesto.FlatAppearance.BorderSize = 0;
+            btnAgregarPuesto.FlatStyle = FlatStyle.Flat;
+            btnAgregarPuesto.Font = new Font("Bahnschrift", 10.2F, FontStyle.Bold);
+            btnAgregarPuesto.ForeColor = Color.White;
+            btnAgregarPuesto.Location = new Point(234, 523);
             btnAgregarPuesto.Margin = new Padding(3, 4, 3, 4);
             btnAgregarPuesto.Name = "btnAgregarPuesto";
-            btnAgregarPuesto.Size = new Size(159, 31);
+            btnAgregarPuesto.Size = new Size(249, 40);
             btnAgregarPuesto.TabIndex = 10;
             btnAgregarPuesto.Text = "Puestos";
-            btnAgregarPuesto.UseVisualStyleBackColor = true;
+            btnAgregarPuesto.UseVisualStyleBackColor = false;
             btnAgregarPuesto.Click += btnAgregarPuesto_Click;
             // 
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.Back_Arrow_svg;
-            pictureBox1.Location = new Point(14, 16);
+            pictureBox1.Location = new Point(16, 15);
             pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(48, 67);
@@ -132,18 +164,30 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label3.Location = new Point(174, 29);
+            label3.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold);
+            label3.Location = new Point(189, 29);
             label3.Name = "label3";
-            label3.Size = new Size(311, 41);
+            label3.Size = new Size(319, 41);
             label3.TabIndex = 12;
             label3.Text = "Gestión de Categorias";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.Payroll;
+            pictureBox2.Location = new Point(634, 11);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(84, 99);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 31;
+            pictureBox2.TabStop = false;
             // 
             // AgregarCategoria
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(663, 600);
+            BackColor = Color.White;
+            ClientSize = new Size(718, 600);
+            Controls.Add(pictureBox2);
             Controls.Add(label3);
             Controls.Add(pictureBox1);
             Controls.Add(btnAgregarPuesto);
@@ -160,6 +204,7 @@
             MouseClick += AgregarCategoria_MouseClick;
             ((System.ComponentModel.ISupportInitialize)dgvCategoria).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -176,5 +221,6 @@
         private Button btnAgregarPuesto;
         private PictureBox pictureBox1;
         private Label label3;
+        private PictureBox pictureBox2;
     }
 }
--- Payroll_1/Formularios/Admin/GestionarEmpleados.Designer.cs
+++ Payroll_1/Formularios/Admin/GestionarEmpleados.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             txt_DUI = new TextBox();
             label1 = new Label();
             label2 = new Label();
@@ -51,14 +53,16 @@
             panel1 = new Panel();
             pictureBox1 = new PictureBox();
             btn_reporte = new Button();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)dgvEmpleados).BeginInit();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // txt_DUI
             // 
-            txt_DUI.Location = new Point(287, 94);
+            txt_DUI.Location = new Point(287, 96);
             txt_DUI.Name = "txt_DUI";
             txt_DUI.Size = new Size(291, 27);
             txt_DUI.TabIndex = 0;
@@ -67,54 +71,60 @@
             // label1
             // 
             label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label1.Location = new Point(60, 101);
             label1.Name = "label1";
-            label1.Size = new Size(37, 20);
+            label1.Size = new Size(40, 20);
             label1.TabIndex = 6;
             label1.Text = "DUI:";
             // 
             // label2
             // 
             label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label2.Location = new Point(60, 426);
             label2.Name = "label2";
-            label2.Size = new Size(144, 20);
+            label2.Size = new Size(153, 20);
             label2.TabIndex = 7;
             label2.Text = "N° Cuenta Corriente:";
             // 
             // label3
             // 
             label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label3.Location = new Point(60, 358);
             label3.Name = "label3";
-            label3.Size = new Size(75, 20);
+            label3.Size = new Size(78, 20);
             label3.TabIndex = 8;
             label3.Text = "Dirección:";
             // 
             // label4
             // 
             label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label4.Location = new Point(60, 293);
             label4.Name = "label4";
-            label4.Size = new Size(70, 20);
+            label4.Size = new Size(74, 20);
             label4.TabIndex = 9;
             label4.Text = "Teléfono:";
             // 
             // label5
             // 
             label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label5.Location = new Point(60, 232);
             label5.Name = "label5";
-            label5.Size = new Size(75, 20);
+            label5.Size = new Size(78, 20);
             label5.TabIndex = 10;
             label5.Text = "Apellidos:";
             // 
             // label6
             // 
             label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label6.Location = new Point(60, 160);
             label6.Name = "label6";
-            label6.Size = new Size(67, 20);
+            label6.Size = new Size(71, 20);
             label6.TabIndex = 11;
             label6.Text = "Nombre:";
             // 
@@ -150,36 +160,56 @@
             // 
             // txt_Nombre
             // 
-            txt_Nombre.Location = new Point(287, 153);
+            txt_Nombre.Location = new Point(287, 158);
             txt_Nombre.Name = "txt_Nombre";
             txt_Nombre.Size = new Size(291, 27);
             txt_Nombre.TabIndex = 16;
             // 
             // btn_Guardar
             // 
+            btn_Guardar.BackColor = Color.FromArgb(135, 115, 238);
+            btn_Guardar.FlatAppearance.BorderSize = 0;
+            btn_Guardar.FlatStyle = FlatStyle.Flat;
+            btn_Guardar.Font = new Font("Bahnschrift", 10.2F, FontStyle.Bold);
+            btn_Guardar.ForeColor = Color.White;
             btn_Guardar.Location = new Point(49, 661);
             btn_Guardar.Name = "btn_Guardar";
             btn_Guardar.Size = new Size(147, 45);
             btn_Guardar.TabIndex = 17;
             btn_Guardar.Text = "Guardar";
-            btn_Guardar.UseVisualStyleBackColor = true;
+            btn_Guardar.UseVisualStyleBackColor = false;
             btn_Guardar.Click += button1_Click;
-           // btn_Guardar.MouseHover += btn_Guardar_MouseHover;
             // 
             // btn_Modificar
             // 
+            btn_Modificar.BackColor = Color.FromArgb(135, 115, 238);
+            btn_Modificar.FlatAppearance.BorderSize = 0;
+            btn_Modificar.FlatStyle = FlatStyle.Flat;
+            btn_Modificar.Font = new Font("Bahnschrift", 10.2F, FontStyle.Bold);
+            btn_Modificar.ForeColor = Color.White;
             btn_Modificar.Location = new Point(231, 661);
             btn_Modificar.Name = "btn_Modificar";
             btn_Modificar.Size = new Size(147, 45);
             btn_Modificar.TabIndex = 18;
             btn_Modificar.Text = "Modificar";
-            btn_Modificar.UseVisualStyleBackColor = true;
+            btn_Modificar.UseVisualStyleBackColor = false;
             btn_Modificar.Click += button2_Click;
             // 
             // dgvEmpleados
             // 
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(183, 171, 240);
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(128, 128, 255);
+            dgvEmpleados.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             dgvEmpleados.BackgroundColor = Color.White;
             dgvEmpleados.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(90, 63, 230);
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dgvEmpleados.DefaultCellStyle = dataGridViewCellStyle2;
             dgvEmpleados.Location = new Point(21, 53);
             dgvEmpleados.Name = "dgvEmpleados";
             dgvEmpleados.RowHeadersWidth = 51;
@@ -198,9 +228,10 @@
             // label7
             // 
             label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label7.Location = new Point(60, 501);
             label7.Name = "label7";
-            label7.Size = new Size(57, 20);
+            label7.Size = new Size(60, 20);
             label7.TabIndex = 22;
             label7.Text = "Correo:";
             // 
@@ -214,21 +245,27 @@
             // label8
             // 
             label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
             label8.Location = new Point(60, 578);
             label8.Name = "label8";
-            label8.Size = new Size(86, 20);
+            label8.Size = new Size(92, 20);
             label8.TabIndex = 24;
             label8.Text = "Contraseña:";
             // 
             // btnGenerarContrasena
             // 
-            btnGenerarContrasena.Location = new Point(195, 570);
+            btnGenerarContrasena.BackColor = Color.FromArgb(135, 115, 238);
+            btnGenerarContrasena.FlatAppearance.BorderSize = 0;
+            btnGenerarContrasena.FlatStyle = FlatStyle.Flat;
+            btnGenerarContrasena.Font = new Font("Bahnschrift", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnGenerarContrasena.ForeColor = Color.White;
+            btnGenerarContrasena.Location = new Point(190, 572);
             btnGenerarContrasena.Margin = new Padding(3, 4, 3, 4);
             btnGenerarContrasena.Name = "btnGenerarContrasena";
             btnGenerarContrasena.Size = new Size(86, 31);
             btnGenerarContrasena.TabIndex = 26;
             btnGenerarContrasena.Text = "Generar";
-            btnGenerarContrasena.UseVisualStyleBackColor = true;
+            btnGenerarContrasena.UseVisualStyleBackColor = false;
             btnGenerarContrasena.Click += btnGenerarContrasena_Click;
             // 
             // panel1
@@ -255,19 +292,36 @@
             // 
             // btn_reporte
             // 
+            btn_reporte.BackColor = Color.FromArgb(135, 115, 238);
+            btn_reporte.FlatAppearance.BorderSize = 0;
+            btn_reporte.FlatStyle = FlatStyle.Flat;
+            btn_reporte.Font = new Font("Bahnschrift", 10.2F, FontStyle.Bold);
+            btn_reporte.ForeColor = Color.White;
             btn_reporte.Location = new Point(416, 661);
             btn_reporte.Name = "btn_reporte";
             btn_reporte.Size = new Size(147, 45);
             btn_reporte.TabIndex = 29;
             btn_reporte.Text = "Reporte";
-            btn_reporte.UseVisualStyleBackColor = true;
+            btn_reporte.UseVisualStyleBackColor = false;
             btn_reporte.Click += btn_reporte_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.Payroll;
+            pictureBox2.Location = new Point(489, 5);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(89, 82);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 30;
+            pictureBox2.TabStop = false;
             // 
             // GestionarEmpleados
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(1835, 735);
+            Controls.Add(pictureBox2);
             Controls.Add(btn_reporte);
             Controls.Add(btnGenerarContrasena);
             Controls.Add(pictureBox1);
@@ -296,6 +350,7 @@
             ((System.ComponentModel.ISupportInitialize)dgvEmpleados).EndInit();
             panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -325,5 +380,6 @@
         private Panel panel1;
         private PictureBox pictureBox1;
         private Button btn_reporte;
+        private PictureBox pictureBox2;
     }
 }
